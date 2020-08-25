55a270f9ec0365dcad277b59e5250a03a9fc95c4
Albert Weinert <info@der-albert.com>
2020-08-25 14:45:15 +0200

    initial project structure

---
+++ src/Invoices.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Invoices.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ webapp/vue.config.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+module.exports = {
+  devServer: {
+    host: "localhost",
+    https: {
+      key: fs.readFileSync("./artifacts/tls/localhost.key"),
+      cert: fs.readFileSync("./artifacts/tls/localhost.crt")
+    },
+    port: 5002,
+    proxy: {
+      "/api": {
+        target: "https://localhost:5001",
+        xfwd: true
+      },
+      "/security": {
+        target: "https://localhost:5001",
+        xfwd: true
+      },
+      "/signin-": {
+        target: "https://localhost:5001",
+        xfwd: true
+      }
+    }
+  }
+};
