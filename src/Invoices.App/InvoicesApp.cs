3c9aa5fa0f8c151559c6dfd8ac7e2b91dbb87a07
Albert Weinert <info@der-albert.com>
2020-09-06 22:12:15 +0200

    added console

---
+++ src/Invoices.App/InvoicesApp.cs
@@ -0,0 +1,58 @@
+using System;
+using Terminal.Gui;
+
+namespace Invoices.App
+{
+    public class InvoicesApp : Toplevel
+    {
+        public InvoicesApp()
+        {
+            ColorScheme = new ColorScheme();
+            var menu = new MenuBar (new[] {
+                new MenuBarItem ("_File", new[] {
+                    new MenuItem ("_Quit", "", () => { if (Quit ()) Running = false; })
+                }),
+                new MenuBarItem ("_Edit", new[] {
+                    new MenuItem ("_Copy", "", null),
+                    new MenuItem ("C_ut", "", null),
+                    new MenuItem ("_Paste", "", null)
+                })
+            });
+            Add (menu);
+
+            var login = new Label ("Login: ") { X = 3, Y = 2 };
+            var password = new Label ("Password: ") {
+                X = Pos.Left (login),
+                Y = Pos.Top (login) + 1
+            };
+            var loginText = new TextField ("") {
+                X = Pos.Right (password),
+                Y = Pos.Top (login),
+                Width = 40
+            };
+            var passText = new TextField ("") {
+                Secret = true,
+                X = Pos.Left (loginText),
+                Y = Pos.Top (password),
+                Width = Dim.Width (loginText)
+            };
+
+            // Add some controls,
+            Add (
+                // The ones with my favorite layout system
+                login, password, loginText, passText,
+
+                // The ones laid out like an australopithecus, with absolute positions:
+                new CheckBox (3, 6, "Remember me"),
+                new RadioGroup (3, 8, new [] { "_Personal", "_Company" }),
+                new Button (3, 14, "Ok"),
+                new Button (10, 14, "Cancel"),
+                new Label (3, 18, "Press F9 or ESC plus 9 to activate the menubar"));
+        }
+
+        bool Quit()
+        {
+            return true;
+        }
+    }
+}
---
+++ src/Invoices.App/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Terminal.Gui;
+
+namespace Invoices.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildHost(args).Build().Run();
+        }
+
+        static IHostBuilder BuildHost(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddOptions<ConsoleLifetimeOptions>()
+                        .Configure(o =>
+                        {
+                            o.SuppressStatusMessages = true;
+                        });
+                    services.AddHostedService<Worker>();
+                });
+        }
+    }
+
+    internal class Worker : IHostedService
+    {
+        readonly IHostApplicationLifetime _lifetime;
+        readonly IServiceScopeFactory _scopeFactory;
+
+        public Worker(IHostApplicationLifetime lifetime, IServiceScopeFactory scopeFactory)
+        {
+            _lifetime = lifetime;
+            _scopeFactory = scopeFactory;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var app = ActivatorUtilities.CreateInstance<InvoicesApp>(scope.ServiceProvider);
+                Application.Run(app);
+//                Application.Run<>();
+            }
+            _lifetime.StopApplication();
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
